7a8006f98789c69be97944b098d4b15d080dd6e1
sowmik-sec <sowmik.sec@gmail.com>
2023-08-04 18:15:13 +0600

    package and import

---
+++ src/com/sowmik/packages/a/Greeting.java
@@ -0,0 +1,11 @@
+package com.sowmik.packages.a;
+
+import static com.sowmik.packages.b.Greeting.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        message();
+    }
+
+}
---
+++ src/com/sowmik/packages/b/Greeting.java
@@ -0,0 +1,10 @@
+package com.sowmik.packages.b;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    public static  void message() {
+        System.out.println("This is awesome");
+    }
+}
