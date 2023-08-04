179c84b12a2d8a87afeb79e8794ff3df31446ded
sowmik-sec <sowmik.sec@gmail.com>
2023-08-04 19:54:44 +0600

    singleton class

---
+++ src/com/sowmik/singleton/Singleton.java
@@ -0,0 +1,16 @@
+package com.sowmik.singleton;
+
+public class Singleton {
+    private int num = 0;
+    private Singleton() {
+
+    }
+    private static Singleton instance;
+    public static Singleton getInstance() {
+        // check whether 1 obj only is created or not
+        if(instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
