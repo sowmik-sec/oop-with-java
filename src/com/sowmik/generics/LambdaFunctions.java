ebc991d0f0f6b22b7d37d8bb621a35acfa1e2710
sowmik-sec <sowmik.sec@gmail.com>
2023-08-06 18:37:54 +0600

    lambda function in java

---
+++ src/com/sowmik/generics/LambdaFunctions.java
@@ -0,0 +1,32 @@
+package com.sowmik.generics;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+public class LambdaFunctions {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            arr.add(i+1);
+        }
+//        arr.forEach((item)-> System.out.println(item*2));
+        Consumer<Integer> fun = (item) -> System.out.println(item*2);
+        arr.forEach(fun);
+        Operation sum = (a,b) -> a + b;
+        Operation product = (a,b) -> a * b;
+        Operation subtraction = (a,b) -> a - b;
+        LambdaFunctions myCalculator = new LambdaFunctions();
+        System.out.println(myCalculator.operate(5,3, sum));
+        System.out.println(myCalculator.operate(65,23, subtraction));
+    }
+    private int operate(int a, int b, Operation op) {
+        return op.operation(a,b);
+    }
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
+
+interface Operation {
+    int operation(int a, int b);
+}
